781d76b68224ba05f5682ca38b9566f17c960b39
Dustin Willis Webber <dustin.webber@gmail.com>
2010-09-03 19:56:00 -0400

    Remove resize user menu logic.

--- data/ronin/team/public/js/roninteam.js
+++ data/ronin/team/public/js/roninteam.js
@@ -143,7 +143,6 @@ var RoninTeam = {
 
 jQuery(document).ready(function($) {
 	RoninTeam.growl();
-	RoninTeam.onlineUserMenu();
   RoninTeam.helpers();
   RoninTeam.flashMessages();
   RoninTeam.chat();
